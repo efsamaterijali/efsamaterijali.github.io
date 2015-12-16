d7ec1c0c84db21bff506aa37fe133980c6e8c83c
efsamaterijali <logglog@email.com>
2015-12-16 11:22:30 +0100

    1001 Gotov wsajt
    Uređena verzija nema dalje :D

--- js/script.js
+++ js/script.js
@@ -1,18 +1,29 @@
 $(document).ready(function () {
+    // Show the Modal on load
+    $("#myModal").modal({backdrop: "static"});
+    $("#myModal").modal("show");
+    
+    
+    // Hide the Modal
+    $("#myBtn").click(function(){
+        $("#myModal").modal("hide");
+    });
+    //zavrsen modal
     $( "#sig" ).click(function() {
         
   var n = $("#input").val();
 
 	if (n == "milekitic") {
-		$("#input").fadeOut("fast");
-        $(".nestani").fadeOut("fast");
-        $("#sajt").fadeIn("fast");
-        $("#h").css("color","red");
+		$("#myModal").modal("hide");
+       
 	} 
 	else {
 		$("#h").css("color","red");
 	}
         
-        
+       
+    
+
+
 });
 });
